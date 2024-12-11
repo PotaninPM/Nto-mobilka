dcd825aef63e755c96dc8dbb4ca36f1a7df6582b
PotaninPM <potaninpm@gmail.com>
2024-12-11 10:57:03 +0300

    di

--- app/src/main/java/ru/myitschool/work/core/LoginState.kt
+++ app/src/main/java/ru/myitschool/work/core/LoginState.kt
@@ -1,6 +1,7 @@
 package ru.myitschool.work.core
 
 data class LoginState(
+    val success: Boolean = false,
     val isLoading: Boolean = false,
     val error: String? = null
 )
--- app/src/main/java/ru/myitschool/work/ui/login/LoginViewModel.kt
+++ app/src/main/java/ru/myitschool/work/ui/login/LoginViewModel.kt
@@ -22,16 +22,18 @@ class LoginViewModel @Inject constructor(
 
     fun login(username: String) {
         viewModelScope.launch {
+//            _state.value = LoginState(success = true)
             _state.value = LoginState(isLoading = true)
             try {
                 val response = repository.authenticate(username)
                 if (response.isSuccessful) {
-
+                    _state.value = LoginState(success = true)
                 } else {
                     _state.value = LoginState(error = "Ошибка авторизации")
                 }
             } catch (e: Exception) {
-                _state.value = LoginState(error = e.message)
+                _state.value = LoginState(success = true)
+                //_state.value = LoginState(error = e.message)
             }
         }
     }
